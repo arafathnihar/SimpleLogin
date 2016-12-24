35786fb0c9e7b816f01211e2f070fda78deffe34
arafathnihar <arafathnihar@gmail.com>
2016-12-24 15:55:43 +0530

    db connection fix

--- server/connection.php
+++ server/connection.php
@@ -1,8 +1,16 @@
 <?php
-$config = parse_ini_file('config.ini');
-$db = new PDO(
-    'mysql:host=' . $config['HOST'] . ';dbname=' . $config['DBNAME'] . ';port=' . $config['PORT'],
-    $config['USERNAME'],
-    $config['PASSWORD']);
 
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $config = parse_ini_file('config.ini');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$db = new PDO(
+    'mysql:host=' . $server . ';dbname=' . $db,
+    $username,
+    $cpassword);
+
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
