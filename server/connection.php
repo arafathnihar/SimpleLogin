197c7b42c2a5be4e45bb637f754b3ee97c1889b5
arafathnihar <arafathnihar@gmail.com>
2016-12-24 16:10:41 +0530

    db connection fix

--- server/connection.php
+++ server/connection.php
@@ -10,10 +10,6 @@ $dbname = substr($url["path"], 1);
 try {
     $pdo = new PDO("mysql:host=".$server."; dbname=".$dbname, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    foreach($pdo->query('SELECT title FROM news') as $row) {
-        echo htmlentities($row['title']);
-    }
-    $pdo = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
